057be4b6b8e8b949b2d039452da21d12a88f4706
Andreas <sdanilsson@gmail.com>
2018-07-28 22:03:32 -0700

    Revert "Revert "Section 8 - 102,103,104""
    This reverts commit 41a82cb430498f84f83f826e4add8914457afc9c.

---
+++ Laser Defender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+        SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
