bf0f75871fb659be53fae39a065ca18f04de3bd7
Andreas <sdanilsson@gmail.com>
2018-07-31 00:17:10 -0700

    Projectiles now destroy enemies
    Projectiles destroy enemies
    The enemies have health

---
+++ Laser Defender/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+    public float health = 150;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Projectile missile = collision.gameObject.GetComponent<Projectile>();
+        if(missile){
+            missile.Hit();
+            health -= missile.GetDamage();
+            if (health <= 0){
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
